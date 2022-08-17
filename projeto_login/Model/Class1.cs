c3bcca4458fbc0d2cfbb247659f5a1961a3e50b3
Julyo Hidalgo <julyohidalgo0@gmail.com>
2022-08-17 15:03:47 -0300

    primeiro

---
+++ projeto_login/Model/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projeto_login.Model
+{
+    internal class Class1
+    {
+    }
+}
---
+++ projeto_login/View/login.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace projeto_login.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+            teste.Text = "bom dia";
+        }
+    }
+}
